45a7f87b81d0350b6ef62cbc3593ca1d368e693a
Eketrin <Katebaiko@yandex.ru>
2023-11-27 16:39:42 +0300

    Добавьте файлы проекта.

---
+++ chempionat/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chempionat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Spisok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void But1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void But2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
